403627b4aeeb9a18bba24c87f50f159da459ca11
Jesse Suen <jessesuen@gmail.com>
2018-08-20 01:27:42 -0700

    Support retrieval of S3 "directories"

--- s3/s3.go
+++ s3/s3.go
@@ -1,6 +1,11 @@
 package s3
 
 import (
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
 	"github.com/minio/minio-go"
 	"github.com/minio/minio-go/pkg/credentials"
 	"github.com/pkg/errors"
@@ -19,6 +24,12 @@ type S3Client interface {
 
 	// GetFile downloads a file to a local file path
 	GetFile(bucket, key, path string) error
+
+	// GetDirectory downloads a directory to a local file path
+	GetDirectory(bucket, key, path string) error
+
+	// IsDirectory tests if the key is acting like a s3 directory
+	IsDirectory(bucket, key string) (bool, error)
 }
 
 type S3ClientOpts struct {
@@ -73,10 +84,46 @@ func (s *s3client) PutFile(bucket, key, path string) error {
 
 }
 
+type uploadTask struct {
+	key  string
+	path string
+}
+
+func generatePutTasks(keyPrefix, rootPath string) chan uploadTask {
+	rootPath = filepath.Clean(rootPath) + "/"
+	uploadTasks := make(chan uploadTask)
+	visit := func(localPath string, fi os.FileInfo, err error) error {
+		relPath := strings.TrimPrefix(localPath, rootPath)
+		if fi.IsDir() {
+			return nil
+		}
+		if fi.Mode()&os.ModeSymlink != 0 {
+			return nil
+		}
+		t := uploadTask{
+			key:  path.Join(keyPrefix, relPath),
+			path: localPath,
+		}
+		uploadTasks <- t
+		return nil
+	}
+	go func() {
+		_ = filepath.Walk(rootPath, visit)
+		close(uploadTasks)
+	}()
+	return uploadTasks
+}
+
 // PutDirectory puts a complete directory into a bucket key prefix, with each file in the directory
 // a separate key in the bucket.
 func (s *s3client) PutDirectory(bucket, key, path string) error {
-	return errors.New("not yet implemented")
+	for putTask := range generatePutTasks(key, path) {
+		err := s.PutFile(bucket, putTask.key, putTask.path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // GetFile downloads a file to a local file path
@@ -87,4 +134,49 @@ func (s *s3client) GetFile(bucket, key, path string) error {
 		return errors.WithStack(err)
 	}
 	return nil
+}
+
+// GetDirectory downloads a s3 directory to a local path
+func (s *s3client) GetDirectory(bucket, keyPrefix, path string) error {
+	log.Infof("Getting directory from s3 (endpoint: %s, bucket: %s, key: %s) to %s", s.Endpoint, bucket, keyPrefix, path)
+	keyPrefix = filepath.Clean(keyPrefix) + "/"
+	doneCh := make(chan struct{})
+	defer close(doneCh)
+	objCh := s.minioClient.ListObjectsV2(bucket, keyPrefix, true, doneCh)
+	for obj := range objCh {
+		if obj.Err != nil {
+			return errors.WithStack(obj.Err)
+		}
+		relKeyPath := strings.TrimPrefix(obj.Key, keyPrefix)
+		localPath := filepath.Join(path, relKeyPath)
+		err := s.minioClient.FGetObject(bucket, obj.Key, localPath, minio.GetObjectOptions{})
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
+}
+
+// IsDirectory tests if the key is acting like a s3 directory. This just means it has at least one
+// object which is prefixed with the given key
+func (s *s3client) IsDirectory(bucket, key string) (bool, error) {
+	doneCh := make(chan struct{})
+	defer close(doneCh)
+	objCh := s.minioClient.ListObjectsV2(bucket, key, false, doneCh)
+	for obj := range objCh {
+		if obj.Err != nil {
+			return false, errors.WithStack(obj.Err)
+		}
+		return true, nil
+	}
+	return false, nil
+}
+
+// IsS3ErrCode returns if the supplied error is of a specific S3 error code
+func IsS3ErrCode(err error, code string) bool {
+	err = errors.Cause(err)
+	if minioErr, ok := err.(minio.ErrorResponse); ok {
+		return minioErr.Code == code
+	}
+	return false
 }
