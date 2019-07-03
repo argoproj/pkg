abdc2a0d6535769b56813e28a4818b369f5e6db1
Alex Collins <alex_collins@intuit.com>
2019-07-03 10:53:35 -0700

    Redact ouput

--- exec/exec_test.go
+++ exec/exec_test.go
@@ -15,7 +15,7 @@ func TestRunCommand(t *testing.T) {
 	log.SetLevel(log.DebugLevel)
 	defer log.SetLevel(log.InfoLevel)
 
-	message, err := RunCommand("echo", CmdOpts{}, "hello world")
+	message, err := RunCommand("echo", CmdOpts{Redactor: Redact([]string{"world"})}, "hello world")
 	assert.NoError(t, err)
 	assert.Equal(t, "hello world", message)
 
@@ -23,13 +23,13 @@ func TestRunCommand(t *testing.T) {
 
 	entry := hook.Entries[0]
 	assert.Equal(t, log.InfoLevel, entry.Level)
-	assert.Equal(t, "echo hello world", entry.Message)
+	assert.Equal(t, "echo hello ******", entry.Message)
 	assert.Contains(t, entry.Data, "dir")
 	assert.Contains(t, entry.Data, "execID")
 
 	entry = hook.Entries[1]
 	assert.Equal(t, log.DebugLevel, entry.Level)
-	assert.Equal(t, "hello world\n", entry.Message)
+	assert.Equal(t, "hello ******\n", entry.Message)
 	assert.Contains(t, entry.Data, "duration")
 	assert.Contains(t, entry.Data, "execID")
 }
@@ -39,27 +39,27 @@ func TestRunCommandTimeout(t *testing.T) {
 	log.SetLevel(log.DebugLevel)
 	defer log.SetLevel(log.InfoLevel)
 
-	output, err := RunCommand("sh", CmdOpts{Timeout: 500 * time.Millisecond}, "-c", "echo my-output && echo my-error >&2 && sleep 2")
-	assert.Equal(t, output, "my-output")
-	assert.EqualError(t, err, "`sh -c echo my-output && echo my-error >&2 && sleep 2` timeout after 500ms")
+	output, err := RunCommand("sh", CmdOpts{Timeout: 500 * time.Millisecond}, "-c", "echo hello world && echo my-error >&2 && sleep 2")
+	assert.Equal(t, output, "hello world")
+	assert.EqualError(t, err, "`sh -c echo hello world && echo my-error >&2 && sleep 2` timeout after 500ms")
 
 	assert.Len(t, hook.Entries, 3)
 
 	entry := hook.Entries[0]
 	assert.Equal(t, log.InfoLevel, entry.Level)
-	assert.Equal(t, "sh -c echo my-output && echo my-error >&2 && sleep 2", entry.Message)
+	assert.Equal(t, "sh -c echo hello world && echo my-error >&2 && sleep 2", entry.Message)
 	assert.Contains(t, entry.Data, "dir")
 	assert.Contains(t, entry.Data, "execID")
 
 	entry = hook.Entries[1]
 	assert.Equal(t, log.DebugLevel, entry.Level)
-	assert.Equal(t, "my-output\n", entry.Message)
+	assert.Equal(t, "hello world\n", entry.Message)
 	assert.Contains(t, entry.Data, "duration")
 	assert.Contains(t, entry.Data, "execID")
 
 	entry = hook.Entries[2]
 	assert.Equal(t, log.ErrorLevel, entry.Level)
-	assert.Equal(t, "`sh -c echo my-output && echo my-error >&2 && sleep 2` timeout after 500ms", entry.Message)
+	assert.Equal(t, "`sh -c echo hello world && echo my-error >&2 && sleep 2` timeout after 500ms", entry.Message)
 	assert.Contains(t, entry.Data, "execID")
 }
 
@@ -68,32 +68,33 @@ func TestTrimmedOutput(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "hello world", message)
 }
+
 func TestRunCommandExitErr(t *testing.T) {
 	hook := test.NewGlobal()
 	log.SetLevel(log.DebugLevel)
 	defer log.SetLevel(log.InfoLevel)
 
-	output, err := RunCommand("sh", CmdOpts{}, "-c", "echo my-output && echo my-error >&2 && exit 1")
-	assert.Equal(t, "my-output", output)
-	assert.EqualError(t, err, "`sh -c echo my-output && echo my-error >&2 && exit 1` failed: my-error")
+	output, err := RunCommand("sh", CmdOpts{Redactor: Redact([]string{"world"})}, "-c", "echo hello world && echo my-error >&2 && exit 1")
+	assert.Equal(t, "hello world", output)
+	assert.EqualError(t, err, "`sh -c echo hello world && echo my-error >&2 && exit 1` failed: my-error")
 
 	assert.Len(t, hook.Entries, 3)
 
 	entry := hook.Entries[0]
 	assert.Equal(t, log.InfoLevel, entry.Level)
-	assert.Equal(t, "sh -c echo my-output && echo my-error >&2 && exit 1", entry.Message)
+	assert.Equal(t, "sh -c echo hello ****** && echo my-error >&2 && exit 1", entry.Message)
 	assert.Contains(t, entry.Data, "dir")
 	assert.Contains(t, entry.Data, "execID")
 
 	entry = hook.Entries[1]
 	assert.Equal(t, log.DebugLevel, entry.Level)
-	assert.Equal(t, "my-output\n", entry.Message)
+	assert.Equal(t, "hello ******\n", entry.Message)
 	assert.Contains(t, entry.Data, "duration")
 	assert.Contains(t, entry.Data, "execID")
 
 	entry = hook.Entries[2]
 	assert.Equal(t, log.ErrorLevel, entry.Level)
-	assert.Equal(t, "`sh -c echo my-output && echo my-error >&2 && exit 1` failed: my-error", entry.Message)
+	assert.Equal(t, "`sh -c echo hello ****** && echo my-error >&2 && exit 1` failed: my-error", entry.Message)
 	assert.Contains(t, entry.Data, "execID")
 }
 
@@ -103,7 +104,7 @@ func TestRunCommandErr(t *testing.T) {
 	defer log.SetLevel(log.InfoLevel)
 
 	// this would create a panic
-	output, err := RunCommand("", CmdOpts{})
+	output, err := RunCommand("", CmdOpts{Redactor: Redact([]string{"world"})})
 	assert.Empty(t, output)
 	assert.EqualError(t, err, "fork/exec : no such file or directory")
 }
@@ -114,4 +115,14 @@ func TestRunInDir(t *testing.T) {
 	message, err := RunCommandExt(cmd, CmdOpts{})
 	assert.Nil(t, err)
 	assert.Equal(t, "/", message)
+}
+
+func TestRedact(t *testing.T) {
+	assert.Equal(t, "", Redact(nil)(""))
+	assert.Equal(t, "", Redact([]string{})(""))
+	assert.Equal(t, "", Redact([]string{"foo"})(""))
+	assert.Equal(t, "foo", Redact([]string{})("foo"))
+	assert.Equal(t, "******", Redact([]string{"foo"})("foo"))
+	assert.Equal(t, "****** ******", Redact([]string{"foo", "bar"})("foo bar"))
+	assert.Equal(t, "****** ******", Redact([]string{"foo"})("foo foo"))
 }
