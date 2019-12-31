205b4acdee7ee514d955512b87698db422810b5f
Alex Collins <alexec@users.noreply.github.com>
2019-12-31 10:43:01 -0800

    feat: Support windows (#11)

--- stats/stats_linux.go
+++ stats/stats_linux.go
@@ -1,7 +1,11 @@
 package stats
 
 import (
+	"os"
+	"os/signal"
 	"runtime"
+	"runtime/pprof"
+	"syscall"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -20,12 +24,46 @@ func StartStatsTicker(d time.Duration) {
 
 // RegisterStackDumper spawns a goroutine which dumps stack trace upon a SIGUSR1
 func RegisterStackDumper() {
-	log.Warn("RegisterStackDumper is not supported on windows - noop")
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGUSR1)
+		for {
+			<-sigs
+			LogStats()
+			LogStack()
+		}
+	}()
 }
 
 // RegisterHeapDumper spawns a goroutine which dumps heap profile upon a SIGUSR2
 func RegisterHeapDumper(filePath string) {
-	log.Warn("RegisterHeapDumper is not supported on windows - noop")
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGUSR2)
+		for {
+			<-sigs
+			runtime.GC()
+			if _, err := os.Stat(filePath); err == nil {
+				err = os.Remove(filePath)
+				if err != nil {
+					log.Warnf("could not delete heap profile file: %v", err)
+					return
+				}
+			}
+			f, err := os.Create(filePath)
+			if err != nil {
+				log.Warnf("could not create heap profile file: %v", err)
+				return
+			}
+
+			if err := pprof.WriteHeapProfile(f); err != nil {
+				log.Warnf("could not write heap profile: %v", err)
+				return
+			} else {
+				log.Infof("dumped heap profile to %s", filePath)
+			}
+		}
+	}()
 }
 
 // LogStats logs runtime statistics
