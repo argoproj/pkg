d825373c79ccde67cf2c04d422339aff077d90e2
Yujun Zhang <yujunz@nvidia.com>
2021-03-15 14:24:22 +0800

    feat: use RWMutex in KeyLock to support more use cases
    RWMutex will perform better for read heavy use cases
    
    Signed-off-by: Yujun Zhang <yujunz@nvidia.com>

--- sync/key_lock.go
+++ sync/key_lock.go
@@ -5,21 +5,23 @@ import "sync"
 type KeyLock interface {
 	Lock(key string)
 	Unlock(key string)
+	RLock(key string)
+	RUnlock(key string)
 }
 
 type keyLock struct {
 	guard sync.RWMutex
-	locks map[string]*sync.Mutex
+	locks map[string]*sync.RWMutex
 }
 
 func NewKeyLock() KeyLock {
 	return &keyLock{
 		guard: sync.RWMutex{},
-		locks: map[string]*sync.Mutex{},
+		locks: map[string]*sync.RWMutex{},
 	}
 }
 
-func (l *keyLock) getLock(key string) *sync.Mutex {
+func (l *keyLock) getLock(key string) *sync.RWMutex {
 	l.guard.RLock()
 	if lock, ok := l.locks[key]; ok {
 		l.guard.RUnlock()
@@ -34,7 +36,7 @@ func (l *keyLock) getLock(key string) *sync.Mutex {
 		return lock
 	}
 
-	lock := &sync.Mutex{}
+	lock := &sync.RWMutex{}
 	l.locks[key] = lock
 	l.guard.Unlock()
 	return lock
@@ -46,4 +48,12 @@ func (l *keyLock) Lock(key string) {
 
 func (l *keyLock) Unlock(key string) {
 	l.getLock(key).Unlock()
+}
+
+func (l *keyLock) RLock(key string) {
+	l.getLock(key).RLock()
+}
+
+func (l *keyLock) RUnlock(key string) {
+	l.getLock(key).RUnlock()
 }
