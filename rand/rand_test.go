1bee05d5af800a3b9dbdd7cc6ab86fdd4699eda4
Alex Collins <alexec@users.noreply.github.com>
2021-08-10 16:39:56 -0700

    fix: Use crypto-secure RNG (#44)
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- rand/rand_test.go
+++ rand/rand_test.go
@@ -1,16 +1,15 @@
 package rand
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestRandString(t *testing.T) {
-	ss := RandStringCharset(10, "A")
-	if ss != "AAAAAAAAAA" {
-		t.Errorf("Expected 10 As, but got %q", ss)
-	}
-	ss = RandStringCharset(5, "ABC123")
-	if len(ss) != 5 {
-		t.Errorf("Expected random string of length 10, but got %q", ss)
-	}
+	ss, err := RandString(10)
+	assert.NoError(t, err)
+	assert.Len(t, ss, 10)
+	ss, err = RandString(5)
+	assert.NoError(t, err)
+	assert.Len(t, ss, 5)
 }
